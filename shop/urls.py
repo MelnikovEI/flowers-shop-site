1dcc706620c51a90168f28540d44addb0428b93f
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-08-25 13:45:18 +0300

    Connect card.html to DB

--- shop/urls.py
+++ shop/urls.py
@@ -6,10 +6,11 @@ from shop import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('card/', views.card, name='card'),
+    path('card/<int:product_id>', views.card, name='card'),
+    # path('post/<slug:slug>', views.post_detail, name='post_detail'),
     path('catalog/', views.catalog, name='catalog'),
     path('consultation/', views.consultation, name='consultation'),
-    path('order/', views.order, name='order'),
+    path('order/<int:product_id>', views.order, name='order'),
     path('order-step/', views.order_step, name='order_step'),
     path('quiz/', views.quiz, name='quiz'),
     path('quiz-step/', views.quiz_step, name='quiz-step'),
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from shop.models import Product
 from shop.serializers import ProductSerializer
 
@@ -7,8 +7,11 @@ def index(request):
     return render(request, template_name='index.html', context={})
 
 
-def card(request):
-    return render(request, template_name='card.html', context={})
+def card(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    serializer = ProductSerializer(product)
+    print(serializer.data)
+    return render(request, template_name='card.html', context={'product': serializer.data})
 
 
 def catalog(request):
@@ -21,7 +24,7 @@ def consultation(request):
     return render(request, template_name='consultation.html', context={})
 
 
-def order(request):
+def order(request, product_id):
     return render(request, template_name='order.html', context={})
 
 
