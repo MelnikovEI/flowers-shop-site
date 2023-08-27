cf04a329a053c55c2d3278185e3e10dd025e8205
MelnikovEI <64632765+MelnikovEI@users.noreply.github.com>
2023-08-27 11:49:56 +0300

    Add delivery range to order creation

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,7 +1,7 @@
 from django.db import transaction
 from django.shortcuts import get_object_or_404
 from phonenumber_field.serializerfields import PhoneNumberField
-from rest_framework.fields import CharField
+from rest_framework.fields import CharField, IntegerField, ChoiceField
 from rest_framework.serializers import ModelSerializer, Serializer
 from shop.models import Product, Situation, PriceChoices, Order, ShopUser, UserRole
 from django.contrib.auth.models import User
@@ -29,6 +29,11 @@ class OrderSerializer(Serializer):
     client_name = CharField(max_length=100)
     phone_number = PhoneNumberField(region='RU')
     address = CharField(max_length=100)
+    delivery_time_range = ChoiceField(
+        required=False,
+        choices=Order.DeliveryTimeRange.choices,
+        default=Order.DeliveryTimeRange.AS_POSSIBLE,
+    )
 
     @transaction.atomic
     def create(self, validated_data):
@@ -39,6 +44,9 @@ class OrderSerializer(Serializer):
                 'password': User.objects.make_random_password(),
             }
         )
+        if not created:
+            user.first_name = validated_data['client_name']
+            user.save(update_fields=['first_name'])
         shop_user, created = ShopUser.objects.get_or_create(
             user=user,
             defaults={
@@ -47,7 +55,11 @@ class OrderSerializer(Serializer):
         )
         shop_user.roles.add(get_object_or_404(UserRole, role=1))
         product = get_object_or_404(Product, pk=validated_data['product_id'])
-        order = Order.objects.create(client=shop_user, address=validated_data['address'])
+        order = Order.objects.create(
+            client=shop_user,
+            address=validated_data['address'],
+            delivery_time_range=validated_data['delivery_time_range']
+        )
         order.product.add(product)
         return order
     """
